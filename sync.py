3b73bf22f115ec74c58562c90c0b1cbd2b6e1532
CaraSwift <clswift1969@gmail.com>
2025-03-10 11:42:08 +0000

    Update sync.py

--- sync.py
+++ sync.py
@@ -7,7 +7,7 @@ def get_passwords(db_path):
     """Fetch passwords from the database."""
     conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
-    cursor.execute("SELECT name, type, password FROM Passwords;")
-    passwords = {row[0]: (row[1], row[2]) for row in cursor.fetchall()}
+    cursor.execute("SELECT * FROM users;")
+    users = {row[0]: (row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9]) for row in cursor.fetchall()}
     conn.close()
-    return passwords
+    return users
