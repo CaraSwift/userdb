16e5551284ff50d2e8c5c9d77fa831ac0bf62b64
CaraSwift <clswift1969@gmail.com>
2025-03-10 11:37:41 +0000

    Update sync.py

--- sync.py
+++ sync.py
@@ -1,1 +1,13 @@
-print("Hello, World!")
+import sqlite3
+import sys
+
+DB_PATH = "/home/ubuntu/nvr/users.db"
+
+def get_passwords(db_path):
+    """Fetch passwords from the database."""
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+    cursor.execute("SELECT name, type, password FROM Passwords;")
+    passwords = {row[0]: (row[1], row[2]) for row in cursor.fetchall()}
+    conn.close()
+    return passwords
