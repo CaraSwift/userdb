4c9df6473ebe09863de3a68aebdd20156743bde5
CaraSwift <clswift1969@gmail.com>
2025-03-18 11:32:06 +0000

    change hosts and db

--- watchertest.sh
+++ watchertest.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 DB_PATH="/usr/nvr/unitconfig/db/users.db"
-PLAYBOOK_PATH="/home/ubuntu/userdb/sync_users.yml"
-INVENTORY_PATH="/home/ubuntu/userdb/hosts.ini"
 LOG_FILE="/var/log/db_sync.log"
 
 echo "$(date): Starting DB watcher..." >> "$LOG_FILE"
 
 inotifywait -m -e modify,close_write "$DB_PATH" | while read -r path action file; do
     echo "$(date): Detected change in $DB_PATH - Action: $action" >> "$LOG_FILE"
-    ansible-playbook -i "$INVENTORY_PATH" "$PLAYBOOK_PATH" >> "$LOG_FILE" 2>&1
 done
